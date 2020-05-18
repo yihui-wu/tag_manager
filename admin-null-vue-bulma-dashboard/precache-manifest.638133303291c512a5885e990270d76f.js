288798f32c735651b574e4718c8c802bf85fdbfb
yihui <fanofholmes@gmail.com>
2020-05-18 17:34:13 +0800

    first commit

---
+++ admin-null-vue-bulma-dashboard/precache-manifest.638133303291c512a5885e990270d76f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b061ff99d238a08534",
+    "url": "/admin-null-vue-bulma-dashboard/css/app.960e2c1d.css"
+  },
+  {
+    "revision": "eeaeb4a3eebc670da20483c2c01ac750",
+    "url": "/admin-null-vue-bulma-dashboard/data-sources/clients.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/admin-null-vue-bulma-dashboard/favicon.png"
+  },
+  {
+    "revision": "c2d9783d4fe8fe3b4784785de17fdaf1",
+    "url": "/admin-null-vue-bulma-dashboard/index.html"
+  },
+  {
+    "revision": "17b061ff99d238a08534",
+    "url": "/admin-null-vue-bulma-dashboard/js/app.5b153739.js"
+  },
+  {
+    "revision": "61e4b961f6872561b51d",
+    "url": "/admin-null-vue-bulma-dashboard/js/chunk-vendors.b6fc83f2.js"
+  },
+  {
+    "revision": "c0ad1fbda8ce5c49a387",
+    "url": "/admin-null-vue-bulma-dashboard/js/client-form.de70bb4b.js"
+  },
+  {
+    "revision": "6eb80038c0d89bc20e15",
+    "url": "/admin-null-vue-bulma-dashboard/js/client-form~forms.b907a419.js"
+  },
+  {
+    "revision": "9e7872626b9a50d91dff",
+    "url": "/admin-null-vue-bulma-dashboard/js/forms.d0db018f.js"
+  },
+  {
+    "revision": "a65d31a656784ae8d6ba",
+    "url": "/admin-null-vue-bulma-dashboard/js/profile.93b5b51d.js"
+  },
+  {
+    "revision": "743167ee1bdb947fad76",
+    "url": "/admin-null-vue-bulma-dashboard/js/tables.4a95b8c4.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/admin-null-vue-bulma-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin-null-vue-bulma-dashboard/robots.txt"
+  }
+]);
